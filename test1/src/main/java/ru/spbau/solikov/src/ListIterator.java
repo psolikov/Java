d13ed7607bd1d66b68b33e26c9d843f2ba349aae
Pavel Solikov <psolikov15@gmail.com>
2017-10-11 22:00:35 +0300

    Fixed misssing folder

---
+++ test1/src/main/java/ru/spbau/solikov/src/ListIterator.java
@@ -0,0 +1,15 @@
+package ru.spbau.solikov.src;
+
+import java.util.Iterator;
+
+public class ListIterator implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+}
